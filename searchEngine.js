611e5c5159d96b6cba2bb9f4f4b102ae28673b2d
Fodor Levente <levilevi183@gmail.com>
2019-02-07 20:43:00 +0100

    Added createSearchInputField function for adding a last element for the select element which is the 'All attribute' option

--- searchEngine.js
+++ searchEngine.js
@@ -1,20 +1,23 @@
-
-    function searchAll() {
-        var input, filter, table, tr, td, i, txtValue;
-        input = document.getElementById("myInput");
+      
+      function searchAll() {
+        var input, filter, found, table, tr, td, i, j;
+        input = document.getElementById("myInput2");
         filter = input.value.toUpperCase();
-        table = document.getElementById("myTable");
+        table = document.getElementById("myTable2");
         tr = table.getElementsByTagName("tr");
         for (i = 0; i < tr.length; i++) {
-          td = tr[i].getElementsByTagName("td")[0];
-          if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-              tr[i].style.display = "";
-            } else {
-              tr[i].style.display = "none";
+            td = tr[i].getElementsByTagName("td");
+            for (j = 0; j < td.length; j++) {
+                if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    found = true;
+                }
             }
-          }       
+            if (found) {
+                tr[i].style.display = "";
+                found = false;
+            } else {
+                tr[i].style.display = "none";
+            }
         }
       }
   
@@ -61,17 +64,26 @@
           input.title = "type in a name or something baby";
           inputDiv.appendChild(input);
       }
-  
+      
+      function createAllSelectOption(index){
+        var option = document.createElement("option");
+        option.value = index;
+        option.text = "All attribute";
+        return option;
+      }
+
       function createCustomSelect(headingsList){
+          var i;
           var myDiv = document.getElementById("myDiv");
           var select = document.createElement("select");
           select.id = "Alma";
-          for(var i = 0; i< headingsList.length; i++){
+          for(i = 0; i< headingsList.length; i++){
               var option = document.createElement("option");
               option.value = i;
               option.text = headingsList[i];
               select.appendChild(option);
           }
+          select.appendChild(createAllSelectOption(i+1));
           myDiv.appendChild(select);
       }
       createCustomSelect(getTableHeadings("myTable"));
